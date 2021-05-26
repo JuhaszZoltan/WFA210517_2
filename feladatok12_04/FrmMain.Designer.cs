c5f25a1013044f1cb606e19db6b98660ea98ba12
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-05-26 17:50:55 +0200

    szovegszerkeszto

---
+++ feladatok12_04/FrmMain.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace feladatok12_04
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKekBetu = new System.Windows.Forms.Button();
+            this.lblSzoveg = new System.Windows.Forms.Label();
+            this.btnFeherHatter = new System.Windows.Forms.Button();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.btnBK = new System.Windows.Forms.Button();
+            this.btnKK = new System.Windows.Forms.Button();
+            this.btnJK = new System.Windows.Forms.Button();
+            this.btnNagyBetus = new System.Windows.Forms.Button();
+            this.btnNagyitas = new System.Windows.Forms.Button();
+            this.btnKicsinyites = new System.Windows.Forms.Button();
+            this.btnAlap = new System.Windows.Forms.Button();
+            this.btnKilepes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKekBetu
+            // 
+            this.btnKekBetu.Location = new System.Drawing.Point(13, 322);
+            this.btnKekBetu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKekBetu.Name = "btnKekBetu";
+            this.btnKekBetu.Size = new System.Drawing.Size(191, 61);
+            this.btnKekBetu.TabIndex = 1;
+            this.btnKekBetu.Text = "Kék betű";
+            this.btnKekBetu.UseVisualStyleBackColor = true;
+            this.btnKekBetu.Click += new System.EventHandler(this.btnKekBetu_Click);
+            // 
+            // lblSzoveg
+            // 
+            this.lblSzoveg.Location = new System.Drawing.Point(13, 9);
+            this.lblSzoveg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSzoveg.Name = "lblSzoveg";
+            this.lblSzoveg.Size = new System.Drawing.Size(594, 263);
+            this.lblSzoveg.TabIndex = 2;
+            this.lblSzoveg.Text = "Helló Világ!";
+            // 
+            // btnFeherHatter
+            // 
+            this.btnFeherHatter.Location = new System.Drawing.Point(212, 322);
+            this.btnFeherHatter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFeherHatter.Name = "btnFeherHatter";
+            this.btnFeherHatter.Size = new System.Drawing.Size(191, 61);
+            this.btnFeherHatter.TabIndex = 1;
+            this.btnFeherHatter.Text = "Fehér Háttér";
+            this.btnFeherHatter.UseVisualStyleBackColor = true;
+            this.btnFeherHatter.Click += new System.EventHandler(this.btnFeherHatter_Click);
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Location = new System.Drawing.Point(411, 322);
+            this.btnTorol.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(191, 61);
+            this.btnTorol.TabIndex = 1;
+            this.btnTorol.Text = "Töröl";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            this.btnTorol.Click += new System.EventHandler(this.btnTorol_Click);
+            // 
+            // btnBK
+            // 
+            this.btnBK.Location = new System.Drawing.Point(13, 393);
+            this.btnBK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBK.Name = "btnBK";
+            this.btnBK.Size = new System.Drawing.Size(191, 61);
+            this.btnBK.TabIndex = 1;
+            this.btnBK.Text = "Balközép";
+            this.btnBK.UseVisualStyleBackColor = true;
+            this.btnBK.Click += new System.EventHandler(this.btnBK_Click);
+            // 
+            // btnKK
+            // 
+            this.btnKK.Location = new System.Drawing.Point(212, 393);
+            this.btnKK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKK.Name = "btnKK";
+            this.btnKK.Size = new System.Drawing.Size(191, 61);
+            this.btnKK.TabIndex = 1;
+            this.btnKK.Text = "Közép-közép";
+            this.btnKK.UseVisualStyleBackColor = true;
+            this.btnKK.Click += new System.EventHandler(this.btnKK_Click);
+            // 
+            // btnJK
+            // 
+            this.btnJK.Location = new System.Drawing.Point(411, 393);
+            this.btnJK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnJK.Name = "btnJK";
+            this.btnJK.Size = new System.Drawing.Size(191, 61);
+            this.btnJK.TabIndex = 1;
+            this.btnJK.Text = "Jobbközép";
+            this.btnJK.UseVisualStyleBackColor = true;
+            this.btnJK.Click += new System.EventHandler(this.btnJK_Click);
+            // 
+            // btnNagyBetus
+            // 
+            this.btnNagyBetus.Location = new System.Drawing.Point(13, 464);
+            this.btnNagyBetus.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNagyBetus.Name = "btnNagyBetus";
+            this.btnNagyBetus.Size = new System.Drawing.Size(191, 61);
+            this.btnNagyBetus.TabIndex = 1;
+            this.btnNagyBetus.Text = "Nagybetűs";
+            this.btnNagyBetus.UseVisualStyleBackColor = true;
+            this.btnNagyBetus.Click += new System.EventHandler(this.btnNagyBetus_Click);
+            // 
+            // btnNagyitas
+            // 
+            this.btnNagyitas.Location = new System.Drawing.Point(212, 464);
+            this.btnNagyitas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNagyitas.Name = "btnNagyitas";
+            this.btnNagyitas.Size = new System.Drawing.Size(191, 61);
+            this.btnNagyitas.TabIndex = 1;
+            this.btnNagyitas.Text = "Nagyítás";
+            this.btnNagyitas.UseVisualStyleBackColor = true;
+            this.btnNagyitas.Click += new System.EventHandler(this.btnNagyitas_Click);
+            // 
+            // btnKicsinyites
+            // 
+            this.btnKicsinyites.Location = new System.Drawing.Point(411, 464);
+            this.btnKicsinyites.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKicsinyites.Name = "btnKicsinyites";
+            this.btnKicsinyites.Size = new System.Drawing.Size(191, 61);
+            this.btnKicsinyites.TabIndex = 1;
+            this.btnKicsinyites.Text = "Kicsinyítés";
+            this.btnKicsinyites.UseVisualStyleBackColor = true;
+            this.btnKicsinyites.Click += new System.EventHandler(this.btnKicsinyites_Click);
+            // 
+            // btnAlap
+            // 
+            this.btnAlap.Location = new System.Drawing.Point(13, 535);
+            this.btnAlap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAlap.Name = "btnAlap";
+            this.btnAlap.Size = new System.Drawing.Size(191, 61);
+            this.btnAlap.TabIndex = 1;
+            this.btnAlap.Text = "Alaphelyzet";
+            this.btnAlap.UseVisualStyleBackColor = true;
+            this.btnAlap.Click += new System.EventHandler(this.btnAlap_Click);
+            // 
+            // btnKilepes
+            // 
+            this.btnKilepes.Location = new System.Drawing.Point(411, 535);
+            this.btnKilepes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKilepes.Name = "btnKilepes";
+            this.btnKilepes.Size = new System.Drawing.Size(191, 61);
+            this.btnKilepes.TabIndex = 1;
+            this.btnKilepes.Text = "Kilépés";
+            this.btnKilepes.UseVisualStyleBackColor = true;
+            this.btnKilepes.Click += new System.EventHandler(this.btnKilepes_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 610);
+            this.Controls.Add(this.lblSzoveg);
+            this.Controls.Add(this.btnKilepes);
+            this.Controls.Add(this.btnKicsinyites);
+            this.Controls.Add(this.btnJK);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.btnNagyitas);
+            this.Controls.Add(this.btnKK);
+            this.Controls.Add(this.btnFeherHatter);
+            this.Controls.Add(this.btnAlap);
+            this.Controls.Add(this.btnNagyBetus);
+            this.Controls.Add(this.btnBK);
+            this.Controls.Add(this.btnKekBetu);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmMain";
+            this.Text = "Szöveg formázás";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKekBetu;
+        private System.Windows.Forms.Label lblSzoveg;
+        private System.Windows.Forms.Button btnFeherHatter;
+        private System.Windows.Forms.Button btnTorol;
+        private System.Windows.Forms.Button btnBK;
+        private System.Windows.Forms.Button btnKK;
+        private System.Windows.Forms.Button btnJK;
+        private System.Windows.Forms.Button btnNagyBetus;
+        private System.Windows.Forms.Button btnNagyitas;
+        private System.Windows.Forms.Button btnKicsinyites;
+        private System.Windows.Forms.Button btnAlap;
+        private System.Windows.Forms.Button btnKilepes;
+    }
+}
+
---
+++ feladatok12_04/FrmMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace feladatok12_04
+{
+    public partial class FrmMain : Form
+    {
+
+
+        EredetiTulajdonsagok ep;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            ep = new EredetiTulajdonsagok()
+            {
+                BackColor = lblSzoveg.BackColor,
+                ForeColor = lblSzoveg.ForeColor,
+                Text = lblSzoveg.Text,
+                Font = lblSzoveg.Font,
+                TextAlign = lblSzoveg.TextAlign,
+            };
+
+        }
+
+        private void btnKekBetu_Click(object sender, EventArgs e)
+        {
+            lblSzoveg.ForeColor = Color.Blue;
+        }
+
+        private void btnFeherHatter_Click(object sender, EventArgs e)
+        {
+            lblSzoveg.BackColor = Color.White;
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            lblSzoveg.Text = null;
+        }
+
+        private void btnBK_Click(object sender, EventArgs e)
+        {
+            lblSzoveg.TextAlign = ContentAlignment.MiddleLeft;
+        }
+
+        private void btnKK_Click(object sender, EventArgs e)
+        {
+            lblSzoveg.TextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        private void btnJK_Click(object sender, EventArgs e)
+        {
+            lblSzoveg.TextAlign = ContentAlignment.MiddleRight;
+        }
+
+        private void btnNagyBetus_Click(object sender, EventArgs e)
+        {
+            lblSzoveg.Text = lblSzoveg.Text.ToUpper();
+        }
+
+        private void btnNagyitas_Click(object sender, EventArgs e)
+        {
+            //string fn = lblSzoveg.Font.Name;
+            float fs = lblSzoveg.Font.Size;
+
+            lblSzoveg.Font = new Font(
+                familyName: lblSzoveg.Font.Name, 
+                emSize: ++fs);
+        }
+
+        private void btnKicsinyites_Click(object sender, EventArgs e)
+        {
+            //float fs = lblSzoveg.Font.Size <= 1 ? lblSzoveg.Font.Size : lblSzoveg.Font.Size - 1;
+
+            float fs = lblSzoveg.Font.Size;
+            lblSzoveg.Font = new Font(
+                familyName: lblSzoveg.Font.Name,
+                emSize: fs <= 1 ? fs : --fs);
+        }
+
+        private void btnKilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAlap_Click(object sender, EventArgs e)
+        {
+            //Application.Restart();
+            
+            lblSzoveg.Text = ep.Text;
+            lblSzoveg.TextAlign = ep.TextAlign;
+            lblSzoveg.BackColor = ep.BackColor;
+            lblSzoveg.ForeColor = ep.ForeColor;
+            lblSzoveg.Font = ep.Font;
+        }
+    }
+
+    struct EredetiTulajdonsagok
+    {
+        public Color BackColor { get; set; }
+        public Color ForeColor { get; set; }
+        public Font Font { get; set; }
+        public ContentAlignment TextAlign { get; set; }
+        public string Text { get; set; }
+    }
+
+
+}
